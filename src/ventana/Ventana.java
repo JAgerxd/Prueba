1ccacf4ee1bcecb623dbd7b0f88e108c6f72895a
JAger <ignacio.guerrero.o@gmail.com>
2019-08-21 11:12:17 -0400

    Se agregan Areas de texto

--- src/ventana/Ventana.java
+++ src/ventana/Ventana.java
@@ -31,8 +31,10 @@ public class Ventana extends JFrame {
         colocarPaneles();
         //colocarEtiqueta();
         //colocarBotones();
-        colocarRadioBotones();
-
+        //colocarRadioBotones();
+        //colocaBotonesDeActivacion();
+        //colocarCajaDeTexto();
+        colocarAreasTexto();
 
 
     }
@@ -83,40 +85,36 @@ public class Ventana extends JFrame {
         //Boton 2 - boton de imagen
 
         JButton boton2 = new JButton();
-        boton2.setBounds(100,200,100,40);
+        boton2.setBounds(100, 200, 100, 40);
         boton2.setOpaque(true);
         ImageIcon clickAqui = new ImageIcon("boton.png");
-        boton2.setIcon(new ImageIcon(clickAqui.getImage().getScaledInstance(boton2.getWidth(),boton2.getHeight(),Image.SCALE_SMOOTH)));
+        boton2.setIcon(new ImageIcon(clickAqui.getImage().getScaledInstance(boton2.getWidth(), boton2.getHeight(), Image.SCALE_SMOOTH)));
         //boton2.setBackground(Color.BLUE); //Establecemos el color de fondo del boton
         panel.add(boton2);
 
         //boton 3 - boton de bordes
         JButton boton3 = new JButton();
-        boton3.setBounds(100,300,110,50);
-        boton3.setBorder(BorderFactory.createLineBorder(Color.GREEN,4,true));
+        boton3.setBounds(100, 300, 110, 50);
+        boton3.setBorder(BorderFactory.createLineBorder(Color.GREEN, 4, true));
         panel.add(boton3);
-
-
-
-
 
 
     }
 
-    private void colocarRadioBotones(){
+    private void colocarRadioBotones() {
         JRadioButton radioBoton1 = new JRadioButton("Opcion 1", false);
-        radioBoton1.setBounds(50,100,200,50);
+        radioBoton1.setBounds(50, 100, 200, 50);
         radioBoton1.setEnabled(true);
         radioBoton1.setText("Programacion"); //colocar el nombre en e l radioboton
-        radioBoton1.setFont(new Font("cooper black",0,20));
+        radioBoton1.setFont(new Font("cooper black", 0, 20));
         panel.add(radioBoton1);
 
         JRadioButton radioBoton2 = new JRadioButton("Opcion 2", false);
-        radioBoton2.setBounds(50,150,100,50);
+        radioBoton2.setBounds(50, 150, 100, 50);
         panel.add(radioBoton2);
 
         JRadioButton radioBoton3 = new JRadioButton("Opcion 3", false);
-        radioBoton3.setBounds(50,200,100,50);
+        radioBoton3.setBounds(50, 200, 100, 50);
         panel.add(radioBoton3);
 
         ButtonGroup grupoRadioBotones = new ButtonGroup();
@@ -125,7 +123,55 @@ public class Ventana extends JFrame {
         grupoRadioBotones.add(radioBoton3);
 
 
+    }
+
+    private void colocaBotonesDeActivacion() {
+
+        JToggleButton botonActivacion1 = new JToggleButton("Opcion 1", true);
+        botonActivacion1.setBounds(50, 100, 100, 40);
+        panel.add(botonActivacion1);
+
+        JToggleButton botonActivacion2 = new JToggleButton("Opcion 2", false);
+        botonActivacion2.setBounds(50, 150, 100, 40);
+        panel.add(botonActivacion2);
+
+        JToggleButton botonActivacion3 = new JToggleButton("Opcion 3", false);
+        botonActivacion3.setBounds(50, 200, 100, 40);
+        panel.add(botonActivacion3);
+
+        ButtonGroup grupoBotonesActivados = new ButtonGroup();
+        grupoBotonesActivados.add(botonActivacion1);
+        grupoBotonesActivados.add(botonActivacion2);
+        grupoBotonesActivados.add(botonActivacion3);
+
 
     }
 
+    private void colocarCajaDeTexto() {
+        JTextField cajaTexto = new JTextField("hola", 50);
+        cajaTexto.setBounds(50, 50, 100, 30);
+        cajaTexto.setText("Hola..."); //texto con el que inicializa la caja
+
+        System.out.println("Textto en la caja: " + cajaTexto.getText());
+        panel.add(cajaTexto);
+
+
+    }
+
+    private void colocarAreasTexto() {
+
+        JTextArea areaTexto = new JTextArea();
+        areaTexto.setBounds(20, 20, 300, 200);
+        areaTexto.setText("Escribe el texto aqui...");
+        areaTexto.append("\nEscribe por aqui... ");  //añade mas texto al area
+        areaTexto.setEditable(true); //edita el area de texto
+
+        System.out.println("El texto es " + areaTexto.getText());
+        panel.add(areaTexto);
+
+        //barras de desplazamiento
+
+
+
+    }
 }
